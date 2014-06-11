261847be7dde30ebdb2596ee8d164809c8bce98f
maany <imptodefeat@gmail.com>
2014-06-11 09:26:44 +0530

    Javafx Draft1 Layout

---
+++ src/com/jfx/test/CinematicEditorDraft1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jfx.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author MAYANK
+ */
+public class CinematicEditorDraft1 extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("CinematicEditorDraft1.fxml"));
+        Scene scene = new Scene(parent,600,200);
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("CinematicEditorDraft1");
+    }
+    public static void main(String args[])
+    {
+        launch(args);
+    }
+}
