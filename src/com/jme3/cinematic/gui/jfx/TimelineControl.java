ed2e738cf9aff922dbf7070b890c65ad296d7174
maany <imptodefeat@gmail.com>
2014-06-15 01:01:31 +0530

    Zoom feature added in timeline. issue : javafx slider not smooth when decreasing value

--- src/com/jme3/cinematic/gui/jfx/TimelineControl.java
+++ src/com/jme3/cinematic/gui/jfx/TimelineControl.java
@@ -35,10 +35,18 @@ public class TimelineControl extends VBox {
     private ScrollPane timelineScrollPane;
     @FXML
     private VBox timelineScrollPaneVBox;
-
+    @FXML
+    
+    private Slider zoom;
     private DoubleProperty currentTime = new SimpleDoubleProperty(0);
+    private DoubleProperty magnification = new SimpleDoubleProperty();
+    private DoubleProperty maxMagnification = new SimpleDoubleProperty();
+    private DoubleProperty duration = new SimpleDoubleProperty();
+    private DoubleProperty frameRate = new SimpleDoubleProperty();
+    private DoubleProperty frameWidth = new SimpleDoubleProperty();
     private double endAdjustment = 8; // timebar extra width due to circular thumb of slider
     private double startAdjustment = 6.5;
+
     public TimelineControl() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("timeline_ui_control.fxml"));
         loader.setRoot(this);
@@ -48,6 +56,7 @@ public class TimelineControl extends VBox {
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+        initView();
         //initListeners();
     }
 
@@ -62,15 +71,14 @@ public class TimelineControl extends VBox {
                 timebarTimesliderGroup.setTranslateX(timebarHVal);
             }
         });
-        
-        timebar.valueProperty().addListener(new ChangeListener<Number>(){
 
+        timebar.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> ov, Number t, Number t1) {
                 currentTime.setValue(t1);
             }
         });
-        
+
         currentTime.addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> ov, Number t, Number t1) {
@@ -78,8 +86,8 @@ public class TimelineControl extends VBox {
                     timebar.setValue(t1.doubleValue());
                 }
                 boolean isTimesliderSync;
-                double expectedPosition = (timebar.getWidth()-endAdjustment-startAdjustment) * timebar.getValue() / timebar.getMax();
-                expectedPosition+=startAdjustment;
+                double expectedPosition = (timebar.getWidth() - endAdjustment - startAdjustment) * timebar.getValue() / timebar.getMax();
+                expectedPosition += startAdjustment;
                 isTimesliderSync = timeslider.getTranslateX() == expectedPosition;
                 System.out.println("Expected Position : " + expectedPosition);
                 if (!isTimesliderSync) {
@@ -87,22 +95,27 @@ public class TimelineControl extends VBox {
                 }
             }
         });
-        
+
         enableTimesliderDrag();
+        
+        initView();
     }
-class Delta { double x, currentTime; }
+
+    class Delta {
+
+        double x, currentTime;
+    }
+
     private void enableTimesliderDrag() {
         final Delta dragDelta = new Delta();
-        
-        timeslider.setOnMouseEntered(new EventHandler<MouseEvent>(){
 
+        timeslider.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent t) {
                 timeslider.getScene().setCursor(Cursor.H_RESIZE);
             }
-        
         });
-        
+
         timeslider.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -111,27 +124,64 @@ class Delta { double x, currentTime; }
 
             }
         });
-        
+
         timeslider.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 double newPos = mouseEvent.getX() + dragDelta.x;
-                dragDelta.currentTime = timebar.getMax()*newPos/(timebar.getWidth()-endAdjustment-startAdjustment);
+                dragDelta.currentTime = timebar.getMax() * newPos / (timebar.getWidth() - endAdjustment - startAdjustment);
                 currentTime.set(dragDelta.currentTime);
                 System.out.println("Mouse Dragged");
             }
         });
-        timeslider.setOnMouseExited(new EventHandler<MouseEvent>(){
-
+        timeslider.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent t) {
                 timeslider.getScene().setCursor(Cursor.DEFAULT);
             }
-        
         });
-        
+    }
+    
+    public void resetView() {
+        magnification.set(timebar.getPrefWidth()/timelineScrollPane.getPrefWidth());
+        timelineScrollPaneVBox.setPrefWidth(timelineScrollPaneVBox.getPrefWidth()/magnification.doubleValue());
+        timebar.setPrefWidth(timebar.getPrefWidth()/magnification.doubleValue());
+        magnification.set(1);
+        Integer majorTickUnit = new Integer((int) (zoom.getMax()));
+                    System.out.println("Majot Tick Unit : " + majorTickUnit);
+                timebar.setMajorTickUnit(majorTickUnit);
+    }
+    public void initView() {
+        resetView();
+        maxMagnification.bindBidirectional(zoom.maxProperty());
+        magnification.bindBidirectional(zoom.valueProperty());
+        magnification.addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number t, Number mag) {
+                if (mag.doubleValue() > 0) {
+                    // change current width
+                    double currentWidth = mag.doubleValue() * timelineScrollPane.getWidth();
+                    timelineScrollPaneVBox.setPrefWidth(currentWidth);
+                    timebar.setPrefWidth(currentWidth);
+                    // change ticks
+                    Integer majorTickUnit = new Integer((int) (zoom.getMax() + zoom.getMin() - mag.floatValue()));
+                    System.out.println("Majot Tick Unit : " + majorTickUnit + " Position : " + timebar.getLayoutX());
+                    timebar.setMajorTickUnit(majorTickUnit);
+                    int durationNearestSecond = (int) duration.doubleValue() - (((int) duration.doubleValue()) % ((int) zoom.getValue()));
+                    timebar.setMinorTickCount(100 / majorTickUnit);
+                }
+            }
+        });
+
         
     }
 
+    public DoubleProperty getDuration() {
+        return duration;
+    }
+
+    public DoubleProperty getFrameRate() {
+        return frameRate;
+    }
     
 }
